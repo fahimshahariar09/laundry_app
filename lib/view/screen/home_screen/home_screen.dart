800311582ad5fe52d3cb828e2bba6869496cbeba
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-16 23:53:57 -0700

    first commit

--- lib/view/screen/home_screen/home_screen.dart
+++ lib/view/screen/home_screen/home_screen.dart
@@ -14,70 +14,64 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Obx(()=>homeScreenController.isLoading.isFalse ? Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Row(
-                children: [
-                  ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: homeScreenController.homeList.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: Container(
-                            height: 100,
-                            width: 100,
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  "${homeScreenController.homeList[index].imageurl}",
-                                  height: 80,
-                                  width: 80,
-                                ),
-                                Text("0"),
-                              ],
-                            ),
-                          ),
-                        );
-                      })
-                ],
-              ),
-            ),
-          ) :CircularProgressIndicator()),
-          // SingleChildScrollView(
-          //     scrollDirection: Axis.horizontal,
-          //     child: Obx(() => homeScreenController.isLoading.isFalse
-          //         ? StreamBuilder<Object>(
-          //           stream: null,
-          //           builder: (context, snapshot) {
-          //             return Row(
-          //               children: [
-          //                 ListView.builder(
-          //                     itemCount: homeScreenController.homeList.length,
-          //                     shrinkWrap: true,
-          //                     itemBuilder: (context, index) {
-          //                       return Card(
-          //                         child: SizedBox(
-          //                           height: 150,
-          //                           width: 150,
-          //                           child: Column(
-          //                             children: [
-          //                               Image.asset(
-          //                                   "${homeScreenController.homeList[index].imageurl}"),
-          //                               Text("0"),
-          //                             ],
-          //                           ),
-          //                         ),
-          //                       );
-          //                     }),
-          //               ],
-          //             );
-          //           }
-          //         )
-          //         : CircularProgressIndicator()))
+          Obx(()=>  SizedBox(
+            height: 150,
+            width: 150,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: homeScreenController.homeList.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "${homeScreenController.homeList[index].imageurl}",
+                          height: 80,
+                          width: 80,
+                        ),
+                        Text("0"),
+                      ],
+                    ),
+                  );
+                }),
+          )),
         ],
       ),
     );
   }
+
+
+
+// SingleChildScrollView(
+//     scrollDirection: Axis.horizontal,
+//     child: Obx(() => homeScreenController.isLoading.isFalse
+//         ? StreamBuilder<Object>(
+//           stream: null,
+//           builder: (context, snapshot) {
+//             return Row(
+//               children: [
+//                 ListView.builder(
+//                     itemCount: homeScreenController.homeList.length,
+//                     shrinkWrap: true,
+//                     itemBuilder: (context, index) {
+//                       return Card(
+//                         child: SizedBox(
+//                           height: 150,
+//                           width: 150,
+//                           child: Column(
+//                             children: [
+//                               Image.asset(
+//                                   "${homeScreenController.homeList[index].imageurl}"),
+//                               Text("0"),
+//                             ],
+//                           ),
+//                         ),
+//                       );
+//                     }),
+//               ],
+//             );
+//           }
+//         )
+//         : CircularProgressIndicator()))
 }
