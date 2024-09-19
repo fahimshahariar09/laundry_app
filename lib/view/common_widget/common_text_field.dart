fa277dd55c98dbeeb82cd5f02431b52ed71d4dd1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-18 21:46:55 -0700

    first commit

--- lib/view/common_widget/common_text_field.dart
+++ lib/view/common_widget/common_text_field.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CommonTextField extends StatelessWidget {
-  const CommonTextField({super.key});
+  const CommonTextField({super.key, this.lText, required this.searchController});
+
+  final TextEditingController searchController;
+
+  final String?  lText;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: searchController,
       decoration: InputDecoration(
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-        )
+        ),
+        labelText: lText,
       ),
     );
   }
 }
+
