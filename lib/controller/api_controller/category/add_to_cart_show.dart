3e61c934b2e0913203208147b72b072d502f33e8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-20 21:40:36 -0700

    first commit

--- lib/controller/api_controller/category/add_to_cart_show.dart
+++ lib/controller/api_controller/category/add_to_cart_show.dart
@@ -1,18 +1,18 @@
-// import 'dart:convert';
-// import 'dart:developer';
-//
-// import 'package:laundry/database/category_detalis/add_to_cart_show.dart';
-// import 'package:laundry/model/category_detalis/add_to_cart_show_model.dart';
-//
-// class AddToCartShowService {
-//   static Future<AddToCartShowModel?> addtocartService() async {
-//     try {
-//       AddToCartShowModel addtocart = AddToCartShowModel.fromJson(
-//           jsonDecode(jsonEncode(AddToCartShowList.addtocartShowList)));
-//       return addtocart;
-//     } catch (e) {
-//       log("error $e");
-//     }
-//     return null;
-//   }
-// }
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:laundry/database/category_detalis/add_to_cart_show.dart';
+import 'package:laundry/model/category_detalis/add_to_cart_show_model.dart';
+
+class AddToCartShowService {
+  static Future<AddToCartShowModel?> addtocartService() async {
+    try {
+      AddToCartShowModel addtocart = AddToCartShowModel.fromJson(
+          jsonDecode(jsonEncode(AddToCartShowList.addtocartShowList)));
+      return addtocart;
+    } catch (e) {
+      log("error $e");
+    }
+    return null;
+  }
+}
