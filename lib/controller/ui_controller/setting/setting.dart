32bac6b82df85a732c77b68e75b32bbbad0aa0f7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-22 23:44:00 -0700

    first commit

--- lib/controller/ui_controller/setting/setting.dart
+++ lib/controller/ui_controller/setting/setting.dart
@@ -6,8 +6,33 @@ import 'package:get/get.dart';
 
 class SettingController extends GetxController{
 
-  RxBool isLigthTheme =true.obs;
+  //language
 
+  RxString selectedLanguage = "BD".obs;
+  RxList<String> languageList =<String>[
+    "BD",
+    "US",
+    "India",
+  ].obs;
+
+  changeLanguageFun(){
+    String local = "en";
+    switch(selectedLanguage.value) {
+      case "BD":
+    local = "bn";
+    break;
+    case "India":
+    local = "hi";
+    break;
+      default:
+        local ="en";
+    }
+    Get.updateLocale(Locale(local));
+    }
+  }
+
+  //dark and light
+  RxBool isLigthTheme =true.obs;
   changeThemeFun()async{
     isLigthTheme.value =!isLigthTheme.value;
     Get.changeTheme(!isLigthTheme.value ? ThemeData.light() : ThemeData.dark());
