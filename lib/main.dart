6689e6d585aa0c1863f1df20c65e40bba9a8bcf0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-28 21:28:18 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:laundry/view/screen/order/confram_order.dart';
 import 'package:laundry/view/screen/splash/splash_screen.dart';
 
 void main(){
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  SplashScreen(),
+      home:  ConframOrder(),
     );
   }
 }
--- lib/view/screen/order/confram_order.dart
+++ lib/view/screen/order/confram_order.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:laundry/view/common_widget/common_text.dart';
 
 class ConframOrder extends StatelessWidget {
   const ConframOrder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+     body: Column(
+       children: [
+         CommonText(titel: "Order Successful"),
+       ],
+     ),
+    );
   }
 }
