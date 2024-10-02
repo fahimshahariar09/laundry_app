9cb87ccfefba5befa8a42ef8dbafa1615c8eb8be
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-01 23:22:36 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:laundry/view/screen/home_screen/home_screen.dart';
-import 'package:laundry/view/screen/home_screen/iron_page.dart';
+import 'package:laundry/view/screen/iron/iron_page.dart';
 
 void main(){
   runApp(const MyApp());
