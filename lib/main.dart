ea474666243f12a638543760d71800c4810d1221
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-01 23:16:45 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:laundry/view/screen/home_screen/home_screen.dart';
+import 'package:laundry/view/screen/home_screen/iron_page.dart';
 
 void main(){
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  HomeScreen(),
+      home:  IronPage(),
     );
   }
 }
---
+++ lib/view/screen/home_screen/dry_clean_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DryCleanPage extends StatelessWidget {
+  const DryCleanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+        ],
+      ),
+    );
+  }
+}
