5fb4133d02cb4d61b57d64d89338ade4669ec8df
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-01 23:18:50 -0700

    first commit

--- lib/view/screen/home_screen/wash_page.dart
+++ lib/view/screen/home_screen/wash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry/view/common_widget/common_text.dart';
 
 class WashPage extends StatelessWidget {
   const WashPage({super.key});
@@ -8,7 +9,34 @@ class WashPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-
+          Container(
+            height: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white
+            ),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: Image.asset("assets/icon/dry.png",fit: BoxFit.cover,),
+                  ),
+                ),
+                const SizedBox(width: 30),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CommonText(titel: "titel",fSize: 20,),
+                    CommonText(titel: "subtitel",fSize: 15,),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
