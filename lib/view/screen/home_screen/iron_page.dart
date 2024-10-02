56a8219fff5f4e978be17a68be0d43e218bee555
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-01 23:12:27 -0700

    first commit

--- lib/view/screen/home_screen/iron_page.dart
+++ lib/view/screen/home_screen/iron_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry/view/common_widget/common_text.dart';
 
 class IronPage extends StatelessWidget {
   const IronPage({super.key});
@@ -6,7 +7,40 @@ class IronPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: SizedBox(
+                      height: 80,
+                      width: 80,
+                      child: Image.asset("assets/icon/dry.png",fit: BoxFit.cover,),
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CommonText(titel: "titel",fSize: 20,),
+                      CommonText(titel: "subtitel",fSize: 15,),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
