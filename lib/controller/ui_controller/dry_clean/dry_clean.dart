a39af5b72def10f6eac1c4a1d1d55ea0f6adacb9
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-03 22:39:32 -0700

    first commit

--- lib/controller/ui_controller/dry_clean/dry_clean.dart
+++ lib/controller/ui_controller/dry_clean/dry_clean.dart
@@ -1,19 +1,16 @@
-
 import 'package:get/get.dart';
 import 'package:laundry/controller/api_controller/dry_clean/dry_clean.dart';
 import 'package:laundry/model/dry_clean/dry_clean.dart';
 
-class DryCleanController extends GetxController{
+class DryCleanController extends GetxController {
+  RxList<DryCleanCategories> drycleancategories = <DryCleanCategories>[].obs;
 
-  RxList<DryCleanCategories> drycleancategories =<DryCleanCategories>[].obs;
+  RxBool isLoading = false.obs;
 
-  dryCleanFun() async{
-
+  dryCleanFun() async {
+    isLoading.value = true;
     var dryclean = await DryCleanService.drycleanService();
     drycleancategories.value = dryclean?.drycleancategories ?? [];
-
-
-
+    isLoading.value = false;
   }
-
 }
---
+++ lib/view/screen/dry_clean/widget/dry_clean_view_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:laundry/view/common_widget/common_text.dart';
+
+class DryCleanViewCard extends StatelessWidget {
+  const DryCleanViewCard({
+    super.key,
+    required this.onTap,
+    required this.image,
+    required this.titel,
+    required this.subtitel,
+  });
+
+  final String image;
+  final String titel;
+  final String subtitel;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: SizedBox(
+                height: 80,
+                width: 80,
+                child: Image.asset(image,fit: BoxFit.cover,),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CommonText(titel: titel,fSize: 20,),
+                CommonText(titel: subtitel,fSize: 15,),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
