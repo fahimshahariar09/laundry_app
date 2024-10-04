20d2a9ec189f7ffb594564e7fc69230de4f0adb1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-03 22:36:10 -0700

    first commit

--- lib/controller/ui_controller/dry_clean/dry_clean.dart
+++ lib/controller/ui_controller/dry_clean/dry_clean.dart
@@ -1,8 +1,19 @@
 
 import 'package:get/get.dart';
+import 'package:laundry/controller/api_controller/dry_clean/dry_clean.dart';
 import 'package:laundry/model/dry_clean/dry_clean.dart';
 
 class DryCleanController extends GetxController{
 
   RxList<DryCleanCategories> drycleancategories =<DryCleanCategories>[].obs;
+
+  dryCleanFun() async{
+
+    var dryclean = await DryCleanService.drycleanService();
+    drycleancategories.value = dryclean?.drycleancategories ?? [];
+
+
+
+  }
+
 }
