2a489b97385f35c9e27e43889a520bcb43d4ddc6
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-04 22:34:31 -0700

    first commit

---
+++ lib/database/iron/iron.dart
@@ -0,0 +1,42 @@
+class IronList {
+  static Map<String, dynamic> ironList = {
+    "ironcategories": [
+      {
+        "imageurl":
+        "assets/category/t-shirt.png",
+        "titel": "T-Shirts clothes",
+        "subtitel": "T-shirts,Polo-Shirts"
+      },
+      {
+        "imageurl":
+        "assets/category/shirts.png.png",
+        "titel": "Shirts",
+        "subtitel": "Shirts,more"
+      },
+      {
+        "imageurl":
+        "assets/category/jeans.png.png",
+        "titel": "Suits",
+        "subtitel": "Jeans Pants,Jersy"
+      },
+      {
+        "imageurl":
+        "assets/category/blazer.png",
+        "titel": "Blazer",
+        "subtitel": "Jacket Blazer,Shirts"
+      },
+      {
+        "imageurl":
+        "assets/category/blankets.png",
+        "titel": "Blankets Textiles",
+        "subtitel": "Blankets,Bedsheets,more"
+      },
+      {
+        "imageurl":
+        "assets/category/silk saree.png.png",
+        "titel": "Household Textiles",
+        "subtitel": "Silk Saree,Bedsheets,"
+      }
+    ]
+  };
+}
