bee223e8d2245cd568bb138dd53bf394c157c4b5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-04 23:15:15 -0700

    first commit

---
+++ lib/controller/api_controller/iron/iron.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:laundry/database/iron/iron.dart';
+import 'package:laundry/model/iron/iron.dart';
+
+class IronService {
+  static Future<IronModel?> ironservice() async {
+    try {
+      IronModel iron =
+          IronModel.fromJson(jsonDecode(jsonEncode(IronList.ironList)));
+    } catch (e) {
+      log(" error $e");
+    }
+    return null;
+  }
+}
