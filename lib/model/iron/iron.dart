13a4b612515757599abe9936359b1bc1cd014a2f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-04 22:36:25 -0700

    first commit

---
+++ lib/model/iron/iron.dart
@@ -0,0 +1,44 @@
+class DryCleanModel {
+  List<DryCleanCategories>? drycleancategories;
+
+  DryCleanModel({this.drycleancategories});
+
+  DryCleanModel.fromJson(Map<String, dynamic> json) {
+    if (json['drycleancategories'] != null) {
+      drycleancategories = <DryCleanCategories>[];
+      json['drycleancategories'].forEach((v) {
+        drycleancategories!.add(DryCleanCategories.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (drycleancategories != null) {
+      data['drycleancategories'] = drycleancategories!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class DryCleanCategories {
+  String? imageurl;
+  String? titel;
+  String? subtitel;
+
+  DryCleanCategories({this.imageurl, this.titel, this.subtitel});
+
+  DryCleanCategories.fromJson(Map<String, dynamic> json) {
+    imageurl = json['imageurl'];
+    titel = json['titel'];
+    subtitel = json['subtitel'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['imageurl'] = imageurl;
+    data['titel'] = titel;
+    data['subtitel'] = subtitel;
+    return data;
+  }
+}
