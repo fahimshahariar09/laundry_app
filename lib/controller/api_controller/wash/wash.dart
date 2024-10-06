b0239d70651fa9546c1479fc5dd0b8594be567bc
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-05 23:12:51 -0700

    first commit

--- lib/controller/api_controller/wash/wash.dart
+++ lib/controller/api_controller/wash/wash.dart
@@ -1,10 +1,12 @@
-
+import 'dart:developer';
 
 import 'package:laundry/model/wash/wash.dart';
 
-class WashService{
-  static Future<WashModel?> washservice()async{
-
-
+class WashService {
+  static Future<WashModel?> washservice() async {
+    try {} catch (e) {
+      log("error :$e");
+    }
+    return null;
   }
 }
