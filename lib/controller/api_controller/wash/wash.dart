be219119043675c4a051dfaccc7fbdfae5c4ff35
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-06 23:05:21 -0700

    first commit

--- lib/controller/api_controller/wash/wash.dart
+++ lib/controller/api_controller/wash/wash.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:laundry/database/wash/wash.dart';
 import 'package:laundry/model/wash/wash.dart';
 
 class WashService {
   static Future<WashModel?> washservice() async {
-    try {} catch (e) {
+    try {
+      WashModel wash =
+          WashModel.fromJson(jsonDecode(jsonEncode(WashList.washList)));
+      return wash;
+    } catch (e) {
       log("error :$e");
     }
     return null;
